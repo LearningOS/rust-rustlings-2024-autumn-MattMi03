32f0c3baf326404195773f2ef86d8b8c7709aff8
MattMi <2022091203023@std.uestc.edu.cn>
2024-10-09 11:53:59 +0800

    update

--- exercises/smart_pointers/box1.rs
+++ exercises/smart_pointers/box1.rs
@@ -5,7 +5,7 @@
 // itself another value of the same type. To get around the issue, we can use a
 // `Box` - a smart pointer used to store data on the heap, which also allows us
 // to wrap a recursive type.
-//
+
 // The recursive type we're implementing in this exercise is the `cons list` - a
 // data structure frequently found in functional programming languages. Each
 // item in a cons list contains two elements: the value of the current item and
@@ -18,11 +18,10 @@
 //
 // Execute `rustlings hint box1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 #[derive(PartialEq, Debug)]
 pub enum List {
-    Cons(i32, List),
+    Cons(i32, Box<List>),
     Nil,
 }
 
@@ -35,11 +34,13 @@ fn main() {
 }
 
 pub fn create_empty_list() -> List {
-    todo!()
+    let list = List::Nil;
+    list
 }
 
 pub fn create_non_empty_list() -> List {
-    todo!()
+    let list = List::Cons(1, Box::new(List::Nil));
+    list
 }
 
 #[cfg(test)]
